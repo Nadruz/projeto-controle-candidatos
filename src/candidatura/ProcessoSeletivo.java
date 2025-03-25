1a9bcea587f0ec24146577d6a7977b0637c956c2
Nadruz <juliananadruz@gmail.com>
2025-03-24 23:06:27 -0300

    Candidatos selecionados

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -2,10 +2,24 @@ package candidatura;
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecaoCandidatos();
+        imprimirSelecionados();
     }
+
+    static void imprimirSelecionados () {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
+
+        for(int indice=0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de número " + (indice+1) + " é o " + candidatos[indice]);
+        }
+
+        for(String candidato: candidatos) 
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
+
     static void selecaoCandidatos() {
-        String [] candidatos = {"Felipe", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
 
         int candidatosSelecionados = 0;
         int candidatosAtual = 0;
@@ -22,9 +36,11 @@ public class ProcessoSeletivo {
             candidatosAtual++;
         }
     }
+
     static double valorPretendido(){
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
+
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
 
